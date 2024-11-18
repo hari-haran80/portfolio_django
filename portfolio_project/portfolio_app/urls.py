e5975fa306bd1a63e5cd3f20004b5ca5ad42511e
hari haran <hariharan.a69@gmail.com>
2024-11-18 17:20:24 +0530

    Fixed some bugs in product deployment(static files)

--- portfolio_project/portfolio_app/urls.py
+++ portfolio_project/portfolio_app/urls.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
 from . import views
+from django.conf import settings
+from django.views.static import serve
 
 urlpatterns = [
     
+    re_path(r'^gallery/(?P<path>.*)$', serve, {'document_root': settings.GALLERY_ROOT}),    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.IMAGE_ROOT}),
+    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
+  
     path('', views.home, name = 'home'),
     path('home', views.home, name = 'home'),
     path('about', views.about, name = 'about'),
