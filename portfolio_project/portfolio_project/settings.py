379a25ac9faa610417f67f41dafec06ca15b32e9
hari haran <hariharan.a69@gmail.com>
2025-04-08 12:14:51 +0530

    ready for pushing

--- portfolio_project/portfolio_project/settings.py
+++ portfolio_project/portfolio_project/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = ['*']
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'hariharan.a69@gmail.com'
-EMAIL_HOST_PASSWORD = 'yenu znkh rilh qgqh'
+EMAIL_HOST_PASSWORD = 'pcvx oyxl oyso mquz'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
