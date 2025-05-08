888883b0df564c5a9d8fe44e0d62b90eb8370458
hari haran <hariharan.a69@gmail.com>
2025-05-08 19:50:11 +0530

    health check up added

--- portfolio_app/urls.py
+++ portfolio_app/urls.py
@@ -52,9 +52,9 @@ urlpatterns = [
     
     # ***************** Access Denied *****************
 
-    path('AccessDenied', views.Denied, name = 'Denied')
+    path('AccessDenied', views.Denied, name = 'Denied'),
     
     # ***************** Health Checking *****************
     
-    path('health/', health_check, name='health-check'),
+    path('health/', views.health_check, name='health-check'),
 ]
